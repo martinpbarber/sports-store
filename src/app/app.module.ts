3166eaa5c1556a018714081a529da153aa2a8d81
Martin Barber <martinpbarber@gmail.com>
2022-10-28 09:43:35 -0400

    Create store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { StoreModule } from "./store/store.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule, StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
