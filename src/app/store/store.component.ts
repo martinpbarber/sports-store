b26bb12909c8be995604ea5a87dc64594f49d33b
Martin Barber <martinpbarber@gmail.com>
2022-10-31 08:56:54 -0400

    Add product pagination

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,11 +8,15 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class StoreComponent {
   selectedCategory: string | undefined;
+  productsPerPage = 4;
+  selectedPage = 1;
   
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
@@ -20,6 +24,22 @@ export class StoreComponent {
   }
 
   changeCategory(newCategory?: string) {
+    this.selectedPage = 1;
     this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository
+        .getProducts(this.selectedCategory).length / this.productsPerPage))
+            .fill(0).map((x, i) => i + 1);
   }
 }
